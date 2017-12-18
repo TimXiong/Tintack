e7c8d1d0df66aa60ba75a7317d9e22570f191c03
TimXiong <xiongliang@outlook.com>
2017-12-17 19:48:09 -0800

    my testing

---
+++ mytesting.cs
@@ -0,0 +1,13 @@
+젊은엄마
+
+--version
+
+git config --global user.name "TimXiong"
+git config --global user.email "xiongliang@outlook.com"
+ssh-keygen
+
+／Users/timxiong/.ssh/id_rsa.pub
+
+it's for https only : 
+my personal access key : 
+d39a40636d6db470a030be2985f1f18aa32ab624
